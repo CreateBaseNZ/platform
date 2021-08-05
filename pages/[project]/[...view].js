fecf051387de7dac6cf684ec4da3882433ecd335
Louis <clin750@aucklanduni.ac.nz>
2021-08-05 23:41:20 +1200

    feat: new build

--- pages/[project]/[...view].js
+++ pages/[project]/[...view].js
@@ -22,7 +22,7 @@ const DUMMY_QUERY = {
     caption:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non aliquam augue. Nullam nunc purus, iaculis at congue a, varius vel massa. Suspendisse eget pharetra ipsum. Praesent vulputate ipsum laoreet tempor viverra. Curabitur vehicula bibendum facilisis. Duis tincidunt mauris ac sem imperdiet imperdiet.",
     stacked: true,
-    scenePrefix: "Project_Industrial_1",
+    scenePrefix: "Project_RoboticArm_1",
     runType: "once",
   },
 };
