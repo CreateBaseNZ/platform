0328bb346c6a208d0306b591ea65e73ae447c60e
Louis <clin750@aucklanduni.ac.nz>
2021-08-29 18:32:38 +1200

    feat: faq page

---
+++ data/faq-data.js
@@ -0,0 +1,76 @@
+export default [
+  {
+    header: "Teacher Aid",
+    icon: "class",
+    items: [
+      {
+        q: "Where can I learn about the concepts I'm teaching?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "What can I do to be comfortable teaching this content?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I assess this content?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I access lesson plans",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I access lesson plans",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I access lesson plans",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+    ],
+  },
+  {
+    header: "Accounts",
+    icon: "account_box",
+    items: [
+      {
+        q: "Where can I learn about the concepts I'm Teaching?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "What can I do to be comfortable teaching this content?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I assess this content?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I access lesson plans",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+    ],
+  },
+  {
+    header: "Other",
+    icon: "help_outline",
+    items: [
+      {
+        q: "Where can I learn about the concepts I'm Teaching?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "What can I do to be comfortable teaching this content?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I assess this content?",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+      {
+        q: "How do I access lesson plans",
+        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+      },
+    ],
+  },
+];
