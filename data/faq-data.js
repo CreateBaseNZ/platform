00bc73fa8aee6d40d77f4c68b6b2357af213cafe
Louis <clin750@aucklanduni.ac.nz>
2021-08-29 18:33:57 +1200

    feat: faq data file

--- data/faq-data.js
+++ data/faq-data.js
@@ -4,28 +4,12 @@ export default [
     icon: "class",
     items: [
       {
-        q: "Where can I learn about the concepts I'm teaching?",
-        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+        q: "An example question?",
+        a: "An example answer.",
       },
       {
-        q: "What can I do to be comfortable teaching this content?",
-        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
-      },
-      {
-        q: "How do I assess this content?",
-        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
-      },
-      {
-        q: "How do I access lesson plans",
-        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
-      },
-      {
-        q: "How do I access lesson plans",
-        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
-      },
-      {
-        q: "How do I access lesson plans",
-        a: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec venenatis, nulla nec tempus faucibus, mauris sem interdum arcu, id condimentum est enim et ipsum. Vivamus non bibendum ante, vitae consequat massa. Aenean vitae bibendum metus, sagittis vestibulum purus. Morbi et gravida nibh. Morbi a neque eget ipsum iaculis pretium et eu nibh.",
+        q: "How do I add another question?",
+        a: "Just like this.",
       },
     ],
   },
