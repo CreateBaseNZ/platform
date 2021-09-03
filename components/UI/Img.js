207ae1c18208bc2c9f467c20eb0b4e7c5b695689
Louis <clin750@aucklanduni.ac.nz>
2021-09-03 21:52:13 +1200

    feat: update login

--- components/UI/Img.js
+++ components/UI/Img.js
@@ -4,16 +4,12 @@ import { useState } from "react";
 import classes from "./Img.module.scss";
 
 const Img = (props) => {
-  const [loaded, setLoaded] = useState(false);
-  return (
-    <div className={`${classes.wrapper} ${loaded ? classes.loaded : ""}`}>
-      <Image
-        onLoadingComplete={() => setLoaded(true)}
-        {...props}
-        className={classes.test}
-      />
-    </div>
-  );
+	const [loaded, setLoaded] = useState(false);
+	return (
+		<div className={`${classes.wrapper} ${loaded ? classes.loaded : ""}`} style={props.style}>
+			<Image onLoadingComplete={() => setLoaded(true)} {...props} className={classes.test} />
+		</div>
+	);
 };
 
 export default Img;
