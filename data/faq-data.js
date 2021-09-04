99859952311787bcee0946912457139cffcfee8e
bbur543 <60163811+bbur543@users.noreply.github.com>
2021-09-04 14:18:22 +1200

    FAQ text draft 1

--- data/faq-data.js
+++ data/faq-data.js
@@ -5,35 +5,59 @@ export default [
     items: [
       {
         q: "How do I access lesson plans?",
-        a: ["Answer coming very very soon.", "/landing.png"],
+        a: ["Each Project has its own set of 5-10 lesson plans to guide you through the whole Project. To find lesson plans, click on the Project image at the bottom of the screen that you are interested in and the ‘Lesson Plan’ button will appear. Clicking it will open all the lesson plans for that Project.", 
+          "/faq-1.jpg"],
       },
       {
         q: "How do I use lesson plans in my classroom?",
-        a: ["Answer coming very very soon."],
+        a: ["How you use lesson plans is entirely up to you. Each lesson plan comes structured in a straightforward way and contains everything you need to deliver the Project.", 
+          "If it is your first time delivering a lesson like this, then we recommend following the lesson plan step-by-step.", 
+          "If you feel more confident about the Project, you are also free to adapt the lesson plans to suit your teaching style.",  
+          "There are no restrictions or pre-requisites in place on the platform so feel free to skip over any content as you see fit or add in your own content throughout the Project."],
       },
       {
         q: "How do I use lesson plans during online lessons?",
-        a: ["Answer coming very very soon."],
-      },
-      {
-        q: "How do I use lesson plans in my classroom?",
-        a: ["Answer coming very very soon."],
-      },
-      {
-        q: "How do I assess this content?",
-        a: ["Answer coming very very soon."],
-      },
-      {
-        q: "How long should my students be on the platform vs in class discussions?",
-        a: ["Answer coming very very soon."],
-      },
-      {
-        q: "How do I get my students onto the platform?",
-        a: ["Answer coming very very soon."],
+        a: ["Exactly as you would in a regular class environment!", 
+          "If your school is running structured lessons through video meeting platforms, we recommend using breakout rooms for class discussions. All other tasks can be completed in the lesson as per the lesson plan or as homework activities. If an individual student needs additional help or guidance, you can ask them to temporarily share their screen (potentially in a breakout room) so that you can see what they are doing and guide them more closely.",
+          "If your school is not running regular lessons or has poor turnout, then you can instead skip over the class discussion parts of the lesson plans and assign students individual tasks (e.g. complete the Research step by DD/MM/YYYY). We recommend you use the learning journal to set completion targets for your class and have them share with you their progress by making their learning journals available to you through email or google classroom."],
       },
       {
         q: "What is Flow coding?",
-        a: ["Answer coming very very soon."],
+        a: ["All of our Projects use the Flow programming language purposefully built by CreateBase to enable students and teachers to learn about coding concepts and code solutions to our Projects.",
+          "Flow coding is designed to be easy to learn for teachers and students, being analogous to creating a logic-based flowchart. This is because we want the coding section of each Project to be based around learning logical thinking, information flow, computational skills, and problem-solving, without the need for memorising lots of arbitrary syntaxes.",
+          "If you want to gain some hands-on experience with Flow coding, visit the Create or Improve steps of any Project. Additionally, check out any of the following links (a full introductory tutorial is coming soon):",
+          "https://app.createbase.co.nz/intro-to-flow.pdf",
+          "https://app.createbase.co.nz/explore/comparison-boost",
+          "https://app.createbase.co.nz/explore/conditional-boost"],
+      },
+      {
+        q: "What are learning journals?",
+        a: ["Learning journals are where students document what they’ve done throughout their Project journey.",
+          "Learning journals are found under the Imagine step in each Project and are available as either a Google Doc or Microsoft Word file.",
+          "Like with most content in a Project, learning journals are optional for teachers. If you do not want your students to complete a learning journal or you want to replace them with your own, then you are free to do so. If you are going to do either of these, then make sure that you communicate it clearly to your class. We would also appreciate you sending us a message explaining why you are skipping them so that we can improve our product."],
+      },
+      {
+        q: "How do I get my students onto the platform?",
+        a: ["Every student needs their own CreateBase account. In a future update, they will gain the ability to sign in using their Google account. To get your students on the platform, get them to visit https://app.createbase.co.nz/", 
+          "If your students don’t have CreateBase accounts, tell them to click sign up and fill out each field. If your students already have CreateBase accounts, they can click log in and enter their account details.",
+          "/faq-2.jpg",
+          "Once they are logged in, they will need to navigate to the Browse page and select the Project you’re teaching. In the example below, the Send It Project has been selected. They can then click continue to start the Project.",
+          "/faq-3.jpg"],
+      },
+      {
+        q: "How much time should my students spend on the platform compared to group/class discussions?",
+        a: ["The easy answer is as much as you’re comfortable with. If you prefer class discussions, you may decide you want your students to do all the class discussion activities and even add some of your own. Instead, you may want to be hands-off and skip over class discussions and get your students to complete tasks on the platform as much as possible. There is no correct approach; teach as you please. The platform and lesson plans are here to help as much as required."],
+      },
+      {
+        q: "Where can I learn about the concepts I’m teaching?",
+        a: ["Each lesson plan includes the minimum amount of content you need as a teacher to deliver that Project. If you would like to have extended discussions on any of the topics covered in the Project, you can supplement the Project content with your own research.",
+          "Each Project will include links to additional resources. Reading and understanding this content is optional; however, they are an excellent place to start if you want to learn more about any of the content covered in the Project."],
+      },
+      {
+        q: "What can I do to be comfortable teaching this content?",
+        a: ["Lesson plans contain everything required to teach a Project.",
+          "We recommend going through the Project lesson plan and content on the platform yourself before you start teaching the lesson to familiarise yourself with its content and structure.",
+          "If you want to feel more confident with a Project’s content and topics, then try reading through some of the additional resources supplied throughout each Project’s lesson plan."],
       },
     ],
   },
@@ -43,31 +67,47 @@ export default [
     items: [
       {
         q: "What are the different account types?",
-        a: ["Answer coming very very soon."],
+        a: ["Currently, there is one account type. On the 12th of September, this will be increased to 3 account types;  Admin, Educator and Learner.",
+          "Admin: Admin accounts have access to the entire platform. They are in charge of managing their organisations' users and billing information.",
+          "Educator: These accounts are for all the teachers in your organisation. They have access to Projects, lesson plans and FAQs.",
+          "Learner: Learner accounts are for students and have the most restricted access. These accounts can only access Projects to reduce distractions and keep students on task. Each student in your class needs to create their own learner account to access the platform."],
       },
       {
         q: "What can my account access?",
-        a: ["Answer coming very very soon."],
+        a: ["Admin accounts have access to the entire platform, including their organisation’s admin page.", 
+          "Educator accounts have access to Projects, lesson plans and FAQs",
+          "Learner accounts can only access Projects."],
       },
       {
-        q: "How do I upgrade my account?",
-        a: ["Answer coming very very soon."],
+        q: "How do I upgrade my organisation to the paid version of CreateBase?",
+        a: ["If you are the admin for your organisation, you can upgrade your account from the 18th of October through our website www.createbase.co.nz to gain access to the paid version of the platform."],
+      },
+      {
+        q: "How do I upgrade my educator account to an admin account?",
+        a: ["If you have an educator account and wish to become an admin for your organisation, please ask one of your organisation's current admins. Admin accounts have the ability to upgrade educator accounts in their organisation to admin accounts through the admin console."],
       },
       {
         q: "How do I join an existing organisation?",
-        a: ["Answer coming very very soon."],
+        a: ["To join an organisation, go to My Account > Join an Organisation and enter your code. If you don’t know your code, talk to one of your organisation's admin users. They can generate a code for you from their admin console.",
+          "/landing.png",
+          "/landing.png"],
       },
       {
         q: "How do I register a new organisation?",
-        a: ["Answer coming very very soon."],
+        a: ["At the end of your platform tutorial, there will be a prompt to create or join an organisation. You will need your school email address, school name, and school ID number to register a new organisation.", 
+          "********Check here if you need to delete or change your organisation.*******"],
+      },
+      {
+        q: "What do I do if myself or one of my students has forgotten their account details?",
+        a: ["There is currently no way to reset an account's password (feature coming soon). If you or one of your students has forgotten either your username or password, you will need to create a new CreateBase account."],
       },
       {
         q: "How do I invite other teachers to my organisation?",
-        a: ["Answer coming very very soon."],
+        a: ["Coming soon..."],
       },
       {
         q: "How do I invite students to my organisation?",
-        a: ["Answer coming very very soon."],
+        a: ["Coming soon..."],
       },
     ],
   },
@@ -76,16 +116,14 @@ export default [
     icon: "help_outline",
     items: [
       {
-        q: "Where can I learn more about the concepts I'm Teaching?",
-        a: ["Answer coming very very soon."],
-      },
-      {
-        q: "What additional platform support is available?",
-        a: ["Answer coming very very soon."],
+        q: "What additional support is available from CreateBase?",
+        a: ["Our platform is still under development. If you encounter any issues or things that you think could be optimised, then don’t hesitate to contact us, and we will fix your problem or potentially add your feature/change request to our development pipeline.",
+          "At this stage, we also offer more personal support to our paid users. If you are part of an organisation that has purchased CreateBase Pro, then contact us about the possibility of arranging product walkthroughs, teacher training and one-on-one support."],
       },
       {
         q: "How can I contact CreateBase?",
-        a: ["Answer coming very very soon."],
+        a: ["You can contact us for any reason via email at: admin@createbase.co.nz",
+          "We will respond to any questions, comments or enquiries within one working day."],
       },
     ],
   },
