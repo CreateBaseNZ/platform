c5967366652250168c1b5c807eee8894fd4ae6f4
Louis <clin750@aucklanduni.ac.nz>
2021-09-06 21:24:28 +1200

    feat: browse tabs

--- components/Browse/BrowsePreview.js
+++ components/Browse/BrowsePreview.js
@@ -1,58 +1,97 @@
+import { memo, useState } from "react";
 import { SecondaryButton } from "../UI/Buttons";
 import Link from "next/link";
 
 import classes from "./BrowsePreview.module.scss";
 
-const BrowsePreview = ({
-  project,
-  videoLoaded,
-  setVideoLoaded,
-  paidAccess,
-}) => {
-  return (
-    <div className={classes.preview}>
-      <div className={classes.vidContainer}>
-        <video
-          key={Math.random()}
-          src={`/${project.query}/vid/situation.mp4`}
-          autoPlay={true}
-          muted={true}
-          className={`${classes.vid} ${videoLoaded ? classes.vidLoaded : ""}`}
-          onCanPlay={() => setVideoLoaded(true)}
-        >
-          <source type="video/mp4" />
-        </video>
-      </div>
-      <div className={classes.details}>
-        <h1 className={classes.h1}>{project.name}</h1>
-        <p className={classes.caption}>{project.caption}</p>
-        <div className={classes.btnContainer}>
-          {paidAccess && project.lessonPlan && (
-            <a href={project.lessonPlan} target="_blank">
-              <SecondaryButton
-                className={classes.lessonBtn}
-                mainLabel="Lesson Plan"
-                iconLeft={
-                  <i className="material-icons-outlined">history_edu</i>
-                }
-              />
-            </a>
-          )}
-          <Link href={`/${project.query}`}>
-            <div>
-              <SecondaryButton
-                className={classes.continueBtn}
-                mainLabel="Continue"
-                iconRight={
-                  <i className="material-icons-outlined">play_arrow</i>
-                }
-              />
-            </div>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+const tabs = ["Overview", "Teaching"];
+
+const BrowsePreview = ({ project, videoLoaded, setVideoLoaded, paidAccess }) => {
+	const [tab, setTab] = useState(0);
+
+	const clickHandler = (i) => {
+		setTab(i);
+	};
+
+	return (
+		<div className={classes.preview}>
+			<div className={classes.vidContainer}>
+				<video
+					// key={Math.random()}
+					src={`/${project.query}/vid/situation.mp4`}
+					autoPlay={true}
+					muted={true}
+					className={`${classes.vid} ${videoLoaded ? classes.vidLoaded : ""}`}
+					onCanPlay={() => setVideoLoaded(true)}>
+					<source type="video/mp4" />
+				</video>
+			</div>
+			<div className={classes.details}>
+				<h1 className={classes.h1}>{project.name}</h1>
+				{tab === 0 && (
+					<>
+						<p className={classes.caption}>{project.caption}</p>
+						<div className={classes.btnContainer}>
+							<Link href={`/${project.query}`}>
+								<div>
+									<SecondaryButton className={classes.continueBtn} mainLabel="Continue" iconRight={<i className="material-icons-outlined">play_arrow</i>} />
+								</div>
+							</Link>
+						</div>
+					</>
+				)}
+				{tab === 1 && (
+					<>
+						<div className={`${classes.teachingCaption} ${classes[project.difficulty]}`}>
+							<span>{project.nLessons}</span> lessons <div className={classes.pipe} /> <span>{project.difficulty}</span> level
+						</div>
+						<div className={classes.subjects}>
+							{project.subjects.map((s) => (
+								<div key={s} className={`${classes.tag} ${classes[s.replace(" ", "")]}`}>
+									{s}
+								</div>
+							))}
+						</div>
+						<div className={classes.moreBtnContainer}>
+							<div className={classes.moreBtnWrapper}>
+								Learning outcomes
+								<a href={project.learningOutcome} title="Download" download>
+									<i className="material-icons-outlined">file_download</i>
+								</a>
+								<a href={project.learningOutcome} title="Open in new tab" target="_blank">
+									<i className="material-icons-outlined">launch</i>
+								</a>
+							</div>
+							<div className={classes.divider} />
+							<div className={classes.moreBtnWrapper}>
+								Curriculum alignment
+								<a href={project.curriculumAlignment} title="Download" download>
+									<i className="material-icons-outlined">file_download</i>
+								</a>
+								<a href={project.curriculumAlignment} title="Open in new tab" target="_blank">
+									<i className="material-icons-outlined">launch</i>
+								</a>
+							</div>
+						</div>
+						{project.lessonPlan && (
+							<a href={project.lessonPlan} target="_blank">
+								<SecondaryButton className={classes.lessonBtn} mainLabel="Lesson Plan" iconLeft={<i className="material-icons-outlined">history_edu</i>} />
+							</a>
+						)}
+					</>
+				)}
+			</div>
+			{paidAccess && (
+				<div className={classes.tabContainer}>
+					{tabs.map((t, i) => (
+						<button key={i} className={`${classes.tab} ${tab === i ? classes.active : ""}`} onClick={() => clickHandler(i)}>
+							{t}
+						</button>
+					))}
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default BrowsePreview;
--- data/send-it-data.js
+++ data/send-it-data.js
@@ -5,151 +5,135 @@
 import { comparisonBoostData, conditionalBoostData } from "./explore-data";
 
 export default {
-  name: "Send It",
-  query: "send-it",
-  caption:
-    'In this project, users will automate a jumping game by creating a simple "AI" that is able to exceed human capabilities and achieve as high of a score as possible. This AI will be controlling a robot with the task of delivering a package as fast as possible, automatically jumping over any obstacles that get in its way.',
-  stacked: true,
-  scenePrefix: "Project_Jump_0",
-  runType: "loop",
-  lessonPlan: "/send-it/files/lesson-plan.pdf",
-  situation: {
-    url: "https://youtu.be/iiWiwxxkq2Q",
-    src: "/send-it/vid/situation.mp4",
-    h1: "Dive into the situation by watching this short video.",
-    h2: "What do you think is happening here? Discuss with your peers!",
-    title: "Send It",
-    docs: "https://docs.google.com/document/d/1BiybIT05ANt76b4rw0ArjHVHpN5LXWNxNCjavtnTM3A/edit?usp=sharing",
-    word: "/send-it/files/learning-journal.docx",
-  },
-  defineCaption: [
-    "Explore the advantages and disadvantages of automation and AI by discussing the questions in ONE of these cards with your group. Make sure to write your answers in your own learning journal. If your group finishes early, feel free to try complete a second card as well!",
-    "When every group has finished, your teacher will call you back to discuss your answers and narrow in on the problem that you will be solving.",
-  ],
-  define: [
-    {
-      title: "Types of delivery robots",
-      img: "/send-it/img/types-0.png",
-      url: "/send-it/pdf/delivery.pdf",
-    },
-    {
-      title: "Delivering mail",
-      img: "/send-it/img/mail-0.png",
-      url: "/send-it/pdf/mail.pdf",
-    },
-    {
-      title: "Controlling a robot",
-      img: "/send-it/img/controlling-1.png",
-      url: "/send-it/pdf/controlling.pdf",
-    },
-    {
-      title: "Sensing Sensors",
-      img: "/send-it/img/controlling-0.png",
-      url: "/send-it/pdf/sensors.pdf",
-    },
-    {
-      title: "The ethics of automation",
-      img: "/send-it/img/ethics-0.png",
-      url: "/send-it/pdf/ethics.pdf",
-    },
-  ],
-  researchCaption: [
-    "Work through the five modules below to complete your research.",
-    "Make sure that you understand all of the content as you will need it to create your solution!",
-  ],
-  research: [
-    {
-      type: "video",
-      title: "Flow tutorial",
-      data: {
-        url: "https://youtu.be/2Ndwtpk7iN8",
-        src: "/flow-tut.mp4",
-        h1: "Flow Tutorial",
-        h2: "Get to know your way around Flow",
-        title: "Flow Tutorial",
-      },
-    },
-    {
-      type: "pdf",
-      title: "Introduction to Flow blocks",
-      url: "/intro-to-flow.pdf",
-    },
-    {
-      type: "pdf",
-      title: "Tips & tricks: sensing blocks",
-      url: "/sensing-blocks.pdf",
-    },
-    {
-      type: "tut",
-      title: "How to Send It",
-      items: [
-        {
-          src: "/send-it/vid/tut-1.mp4",
-          subtitle: <p>Run 1000m to deliver the Pizza</p>,
-        },
-        {
-          src: "/send-it/vid/tut-2.mp4",
-          subtitle: (
-            <p>
-              Jump over obstacles to avoid crashing into them
-              <span className="material-icons-outlined">arrow_upward</span>
-            </p>
-          ),
-        },
-        {
-          src: "/send-it/vid/tut-3.mp4",
-          subtitle: (
-            <p>
-              Crouch under flying obstacles to avoid crashing into them
-              <span className="material-icons-outlined">arrow_downward</span>
-            </p>
-          ),
-        },
-        {
-          src: "/send-it/vid/tut-4.mp4",
-          subtitle: (
-            <p>
-              Change the simulation speed to allow more time for your code to
-              react
-            </p>
-          ),
-        },
-      ],
-    },
-    {
-      type: "explore",
-      title: "Explore more",
-      items: [comparisonBoostData, conditionalBoostData],
-    },
-  ],
-  plan: [
-    "Open up your learning journal and answer all of the questions in the Plan section.",
-    "If you get stuck, ask one of your classmates for help but don't forget to explain your own reasoning!",
-  ],
-  create: {
-    caption:
-      "This step is all about building your own code, making sure you test as you go. Rinse and repeat. Be sure to share it with your friends!",
-    tasks: [
-      "Write some code so that your robot can detect incoming obstacles and avoid them",
-      "Reach 1000m to deliver your package and complete the task. Good luck!",
-    ],
-    hints: [
-      "Make sure that you hit the compile button to upload your code to the robot",
-    ],
-  },
-  improve: {
-    caption:
-      "Test what you’ve learnt by taking on more challenges. There are always ways to make your solution smarter, faster, stronger!",
-    alert:
-      "Did you beat the game? Uh oh, looks like there’s now some flying drones! And is that acceleration? Different sized obstacles too? Time to rethink your code...",
-    tasks: [
-      "Modify your code to duck under flying drones",
-      "Modify your code to take into account an accelerating robot",
-      "Modify your code to jump over obstacles of different sizes",
-      "Deliver that package with the highest score possible",
-    ],
-    hints: [
-      "Rather than trying to do solve them all at once, try turning on one modifier, updating your code until it works, and only then adding the next modifier until you have solved them all",
-    ],
-  },
+	name: "Send It",
+	query: "send-it",
+	caption:
+		'In this project, users will automate a jumping game by creating a simple "AI" that is able to exceed human capabilities and achieve as high of a score as possible. This AI will be controlling a robot with the task of delivering a package as fast as possible, automatically jumping over any obstacles that get in its way.',
+	stacked: true,
+	scenePrefix: "Project_Jump_0",
+	runType: "loop",
+	nLessons: 6,
+	difficulty: "Proficient",
+	subjects: ["Technology", "Engineering", "Computer Science", "Automation", "AI", "Ethics"],
+	learningOutcome: "/send-it/TODO.pdf",
+	curriculumAlignment: "/send-it/TODO.pdf",
+	lessonPlan: "/send-it/files/lesson-plan.pdf",
+	situation: {
+		url: "https://youtu.be/iiWiwxxkq2Q",
+		src: "/send-it/vid/situation.mp4",
+		h1: "Dive into the situation by watching this short video.",
+		h2: "What do you think is happening here? Discuss with your peers!",
+		title: "Send It",
+		docs: "https://docs.google.com/document/d/1BiybIT05ANt76b4rw0ArjHVHpN5LXWNxNCjavtnTM3A/edit?usp=sharing",
+		word: "/send-it/files/learning-journal.docx",
+	},
+	defineCaption: [
+		"Explore the advantages and disadvantages of automation and AI by discussing the questions in ONE of these cards with your group. Make sure to write your answers in your own learning journal. If your group finishes early, feel free to try complete a second card as well!",
+		"When every group has finished, your teacher will call you back to discuss your answers and narrow in on the problem that you will be solving.",
+	],
+	define: [
+		{
+			title: "Types of delivery robots",
+			img: "/send-it/img/types-0.png",
+			url: "/send-it/pdf/delivery.pdf",
+		},
+		{
+			title: "Delivering mail",
+			img: "/send-it/img/mail-0.png",
+			url: "/send-it/pdf/mail.pdf",
+		},
+		{
+			title: "Controlling a robot",
+			img: "/send-it/img/controlling-1.png",
+			url: "/send-it/pdf/controlling.pdf",
+		},
+		{
+			title: "Sensing Sensors",
+			img: "/send-it/img/controlling-0.png",
+			url: "/send-it/pdf/sensors.pdf",
+		},
+		{
+			title: "The ethics of automation",
+			img: "/send-it/img/ethics-0.png",
+			url: "/send-it/pdf/ethics.pdf",
+		},
+	],
+	researchCaption: ["Work through the five modules below to complete your research.", "Make sure that you understand all of the content as you will need it to create your solution!"],
+	research: [
+		{
+			type: "video",
+			title: "Flow tutorial",
+			data: {
+				url: "https://youtu.be/2Ndwtpk7iN8",
+				src: "/flow-tut.mp4",
+				h1: "Flow Tutorial",
+				h2: "Get to know your way around Flow",
+				title: "Flow Tutorial",
+			},
+		},
+		{
+			type: "pdf",
+			title: "Introduction to Flow blocks",
+			url: "/intro-to-flow.pdf",
+		},
+		{
+			type: "pdf",
+			title: "Tips & tricks: sensing blocks",
+			url: "/sensing-blocks.pdf",
+		},
+		{
+			type: "tut",
+			title: "How to Send It",
+			items: [
+				{
+					src: "/send-it/vid/tut-1.mp4",
+					subtitle: <p>Run 1000m to deliver the Pizza</p>,
+				},
+				{
+					src: "/send-it/vid/tut-2.mp4",
+					subtitle: (
+						<p>
+							Jump over obstacles to avoid crashing into them
+							<span className="material-icons-outlined">arrow_upward</span>
+						</p>
+					),
+				},
+				{
+					src: "/send-it/vid/tut-3.mp4",
+					subtitle: (
+						<p>
+							Crouch under flying obstacles to avoid crashing into them
+							<span className="material-icons-outlined">arrow_downward</span>
+						</p>
+					),
+				},
+				{
+					src: "/send-it/vid/tut-4.mp4",
+					subtitle: <p>Change the simulation speed to allow more time for your code to react</p>,
+				},
+			],
+		},
+		{
+			type: "explore",
+			title: "Explore more",
+			items: [comparisonBoostData, conditionalBoostData],
+		},
+	],
+	plan: ["Open up your learning journal and answer all of the questions in the Plan section.", "If you get stuck, ask one of your classmates for help but don't forget to explain your own reasoning!"],
+	create: {
+		caption: "This step is all about building your own code, making sure you test as you go. Rinse and repeat. Be sure to share it with your friends!",
+		tasks: ["Write some code so that your robot can detect incoming obstacles and avoid them", "Reach 1000m to deliver your package and complete the task. Good luck!"],
+		hints: ["Make sure that you hit the compile button to upload your code to the robot"],
+	},
+	improve: {
+		caption: "Test what you’ve learnt by taking on more challenges. There are always ways to make your solution smarter, faster, stronger!",
+		alert: "Did you beat the game? Uh oh, looks like there’s now some flying drones! And is that acceleration? Different sized obstacles too? Time to rethink your code...",
+		tasks: [
+			"Modify your code to duck under flying drones",
+			"Modify your code to take into account an accelerating robot",
+			"Modify your code to jump over obstacles of different sizes",
+			"Deliver that package with the highest score possible",
+		],
+		hints: ["Rather than trying to do solve them all at once, try turning on one modifier, updating your code until it works, and only then adding the next modifier until you have solved them all"],
+	},
 };
