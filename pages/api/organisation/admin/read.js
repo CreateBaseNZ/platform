07b61d11755a3d30b967b9877b612c02f8ee594d
Carl Velasco <carlvelasco96@gmail.com>
2021-09-09 13:30:40 +1200

    Optimised the admin console read route

--- pages/api/organisation/admin/read.js
+++ pages/api/organisation/admin/read.js
@@ -9,23 +9,26 @@ export default async function (req, res) {
 	if (req.method !== "POST") return;
 	// Validate PUBLIC_API_KEY
 	if (req.body.PUBLIC_API_KEY !== process.env.PUBLIC_API_KEY) {
-		return res.send({ status: "critical error", content: "Invalid API key" });
+		return res.send({ status: "critical error", content: "" });
 	}
 	// Check if a session exist
 	const session = await getSession({ req });
 	if (!session) {
-		return res.send({ status: "critical error", content: "This user is not logged in" });
-	}
-	// Validate if the user is an admin
-	if (session.user.access !== "admin" /*|| !session.user.verified*/) {
-		return res.send({ status: "critical error", content: "Invalid access" });
+		// Validate a user is logged in
+		return res.send({ status: "critical error", content: "" });
+	} else if (!session.user.organisation) {
+		// Validate the user is in an organisation
+		return res.send({ status: "critical error", content: "" });
+	} else if (session.user.access !== "admin" /*|| !session.user.verified*/) {
+		// Validate if the user is an admin
+		return res.send({ status: "critical error", content: "" });
 	}
 	// Create the input data
 	const input = { organisation: session.user.organisation };
 	// Send the request
 	let data;
 	try {
-		data = (await axios.post(process.env.ROUTE_URL + "/organisation/admin-read", { PRIVATE_API_KEY: process.env.PRIVATE_API_KEY, input }))["data"];
+		data = (await axios.post(process.env.ROUTE_URL + "/organisation/admin/read", { PRIVATE_API_KEY: process.env.PRIVATE_API_KEY, input }))["data"];
 	} catch (error) {
 		return res.send({ status: "error", content: error });
 	}
