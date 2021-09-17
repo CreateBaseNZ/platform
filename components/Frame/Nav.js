09caf70bf522248f1381911e6bcd1e89da19ab0e
Louis <clin750@aucklanduni.ac.nz>
2021-09-17 14:46:05 +1200

    fix: admin console coming soon

--- components/Frame/Nav.js
+++ components/Frame/Nav.js
@@ -9,23 +9,29 @@ const myAccountTab = { route: "/user/my-account", label: "My Account", icon: "pe
 const adminConsoleTab = { route: "/user/admin-console", label: "Admin Console", icon: "admin_panel_settings" };
 const faqTab = { route: "/faq", label: "FAQ", icon: "help_outline" };
 
+const allTabs = {
+	onboarding: { route: "/onboarding", label: "Onboarding", icon: "skateboarding" },
+	browse: { route: "/browse", label: "Browse", icon: "camera_roll" },
+	myAccount: { route: "/user/my-account", label: "My Account", icon: "person" },
+	adminConsole: { route: "/user/admin-console", label: "Admin Console", icon: "admin_panel_settings" },
+	faq: { route: "/faq", label: "FAQ", icon: "help_outline" },
+};
+
 const tabs = {
-	null: [browseTab, faqTab],
-	learner: [browseTab, faqTab, myAccountTab],
-	educator: [onboardingTab, browseTab, myAccountTab, faqTab],
-	admin: [onboardingTab, browseTab, adminConsoleTab, myAccountTab, faqTab],
+	null: [allTabs.browse, allTabs.faq],
+	learner: [allTabs.browse, allTabs.faq, allTabs.myAccount],
+	educator: [allTabs.onboarding, allTabs.browse, allTabs.myAccount, allTabs.faq],
+	admin: [allTabs.onboarding, allTabs.browse, allTabs.adminConsole, allTabs.myAccount, allTabs.faq],
 };
 
 const Nav = ({ route, collapseNav, user }) => {
-	if (route.startsWith("/faq")) {
-		route = "/faq";
-	} else if (route.startsWith("/browse")) {
-		route = "/browse";
-	} else if (route.startsWith("/user/my-account")) {
-		route = "/user/my-account";
-	} else if (route.startsWith("/user/admin-console")) {
-		route = "/user/admin-console";
+	console.log(tabs[user.type]);
+
+	if (user.loaded) {
+		route = tabs[user.type].find((t) => route.startsWith(t.route)).route || route;
 	}
+
+	console.log(route);
 
 	return (
 		<nav className={`${classes.nav} ${collapseNav ? classes.collapse : ""}`}>
