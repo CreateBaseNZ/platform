292629fbdfc0a34cbbaac1b78a0f79e8035c40fd
Louis <clin750@aucklanduni.ac.nz>
2021-09-22 23:03:41 +1200

    feat: iteration example

--- data/line-following-data.js
+++ data/line-following-data.js
@@ -76,5 +76,58 @@ export default {
 				hints: ["TBD"],
 			},
 		},
+		{
+			define: {
+				caption: [
+					"As a class, dive into group discussions around the Project theme to fully define our problem.",
+					"Don't have a teacher to guide you through? Check back soon for individual content!",
+				],
+				modules: [
+					{
+						title: "TBD",
+						img: "",
+						url: "",
+					},
+					{
+						title: "TBD",
+						img: "",
+						url: "",
+					},
+				],
+			},
+			research: {
+				caption: ["Work through the four modules below to complete your research.", "Make sure that you understand all of the content as you will need it to create your solution!"],
+				modules: [
+					{
+						type: "video",
+						title: "Flow tutorial",
+						data: {
+							url: "https://youtu.be/2Ndwtpk7iN8",
+							src: "/flow-tut.mp4",
+							h1: "Flow Tutorial",
+							h2: "Get to know your way around Flow",
+							title: "Flow Tutorial",
+						},
+					},
+					{
+						type: "pdf",
+						title: "Introduction to Flow blocks",
+						url: "/intro-to-flow.pdf",
+					},
+				],
+			},
+			plan: ["TBD"],
+			create: {
+				caption: "This step is all about building your own code, making sure you test as you go. Rinse and repeat. Be sure to share it with your friends!",
+				tasks: ["TBD"],
+				hints: ["TBD"],
+			},
+			improve: {
+				caption: "Test what you’ve learnt by taking on more challenges. There are always ways to make your solution smarter, faster, stronger!",
+				alert: "TBD",
+				tasks: ["TBD"],
+				hints: ["TBD"],
+			},
+		},
 	],
 };
