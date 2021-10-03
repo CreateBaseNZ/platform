a4480faa60ba1bc1600901d7268c11b0bba8d9ff
Louis <clin750@aucklanduni.ac.nz>
2021-10-03 18:47:44 +1300

    fix: added loading screen back in

--- components/Project/Define.js
+++ components/Project/Define.js
@@ -4,16 +4,24 @@ import ModuleContainer from "../UI/ModuleContainer";
 
 const Define = ({ data }) => {
 	const [active, setActive] = useState(0);
+	const [loaded, setLoaded] = useState(false);
 
 	const cardClickHandler = (i) => {
-		setActive(i);
+		console.log(i);
+		if (i !== active) {
+			setLoaded(false);
+			setActive(i);
+		}
 	};
+
+	const loadHandler = () => setLoaded(true);
 
 	return (
 		<div className={classes.view}>
 			<ModuleContainer active={active} clickHandler={cardClickHandler} modules={data.modules} caption={data.caption} />
 			<div className={classes.mainContainer}>
-				<embed src={data.modules[active].url} width="100%" height="100%" />
+				<embed src={data.modules[active].url} width="100%" height="100%" onLoad={loadHandler} />
+				<div className={`${classes.loadScreen} ${loaded ? classes.loaded : ""}`} />
 			</div>
 		</div>
 	);
