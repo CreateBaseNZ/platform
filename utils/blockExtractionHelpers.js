dc9f545816a739c27467f683962231c5780c5247
bo75salim <47123349+bo75salim@users.noreply.github.com>
2021-11-09 20:19:14 +1300

    gg

--- utils/blockExtractionHelpers.js
+++ utils/blockExtractionHelpers.js
@@ -350,6 +350,8 @@ export const defineObject = (projectName) => {
 			return "Arm";
 		case "heat-seeker":
 			return "Car";
+		case "aimbot":
+			return "aimbot";
 	}
 	return "";
 };
@@ -358,6 +360,7 @@ export const isOnceCode = (projectName) => {
 	switch (projectName) {
 		case "send-it":
 		case "heat-seeker":
+		case "aimbot":
 			return false;
 		case "magnebot":
 			return true;
