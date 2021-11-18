a7cb585d432772ac36b2c03e5cf53b6eec72fd98
Louis <clin750@aucklanduni.ac.nz>
2021-11-18 18:43:30 +1300

    feat: onboarding tasks

--- components/Onboarding/OnboardingTask.js
+++ components/Onboarding/OnboardingTask.js
@@ -1,27 +1,28 @@
-import Img from "../../components/UI/Img";
-import classes from "./Task.module.scss";
+import Img from "../UI/Img";
+import classes from "./OnboardingTask.module.scss";
 
-const Task = ({ task, isCompleted, checkHandler }) => {
-	console.log(task.imgUrl);
-
+const OnboardingTask = ({ task, isCompleted, checkHandler }) => {
 	return (
-		<div className={classes.task}>
+		<div className={`${classes.task} ${isCompleted ? classes.completed : ""}`}>
 			<div className={classes.imgContainer}>
 				<Img style={{ height: "100%", width: "100%" }} src={task.imgUrl} layout="fill" objectFit="cover" />
 			</div>
 			<div className={classes.cover} style={{ backgroundColor: task.color }} />
-			<button className={classes.status} onClick={() => checkHandler(task.id)}>
+			<button className={classes.status} onClick={checkHandler}>
 				{isCompleted ? "Completed" : "Mark complete"}
 				<div className={`${classes.checkbox} ${isCompleted ? classes.checked : ""}`}>{isCompleted && <i className="material-icons-outlined">check</i>}</div>
 			</button>
 			{task.type === "video" && <i className={`material-icons-outlined ${classes.icon}`}>play_circle_outline</i>}
-			{task.type === "text" && <i className={`material-icons-outlined ${classes.icon}`}>stick_note_2</i>}
+			{task.type === "text" && <i className={`material-icons-outlined ${classes.icon}`}>sticky_note_2</i>}
 			<div className={classes.titleContainer}>
-				<h2>{task.title}</h2>
+				<div className={classes.titleWrapper}>
+					<div className={classes.subtitle}>{task.subtitle}</div>
+					<h2>{task.title}</h2>
+				</div>
 				{task.type === "link" && <i className={`material-icons-outlined ${classes.icon}`}>arrow_forward</i>}
 			</div>
 		</div>
 	);
 };
 
-export default Task;
+export default OnboardingTask;
