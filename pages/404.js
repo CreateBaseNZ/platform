a9d552d99ce6c2593cb7c04ff58389e43458ec23
Louis <clin750@aucklanduni.ac.nz>
2021-11-23 00:05:29 +1300

    feat: 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,32 @@
 import Head from "next/head";
+import { PrimaryButton } from "../components/UI/Buttons";
+import Link from "next/link";
+import Img from "../components/UI/Img";
+
+import classes from "../styles/404.module.scss";
 
 const Error404 = () => {
 	return (
-		<div>
+		<div className={classes.view}>
 			<Head>
 				<title>Error 404 | CreateBase</title>
-				<meta name="description" content="Log into your CreateBase account" />
+				<meta name="description" content="Oops!" />
 			</Head>
-			Error 404
+			<div className={classes.container}>
+				<div className={classes.heading}>
+					<h1>404</h1>
+					<div className={classes.img}>
+						<Img src="https://raw.githubusercontent.com/CreateBaseNZ/public/main/404.png" layout="fill" objectFit="contain" />
+					</div>
+				</div>
+				<h2>Oops! You’ve found a magical land with polygon chickens.</h2>
+				<h3>The page you’re looking for doesn’t exist or has been removed. Poof! You didn’t see anything...</h3>
+			</div>
+			<Link href="/" replace={true}>
+				<div>
+					<PrimaryButton mainLabel="Take me home" className={classes.btn} />
+				</div>
+			</Link>
 		</div>
 	);
 };
