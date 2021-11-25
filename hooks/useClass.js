79af48242b686b5e1b48a320232517b0918fd0a3
Louis <clin750@aucklanduni.ac.nz>
2021-11-26 00:31:33 +1300

    chore: updating more routes

--- hooks/useClass.js
+++ hooks/useClass.js
@@ -1,39 +1,29 @@
 import router from "next/router";
-import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import GlobalSessionContext from "../store/global-session-context";
-import useHandleResponse from "./useHandleResponse";
+import useApi from "./useApi";
 
 const useClass = () => {
+	const post = useApi();
 	const { globalSession } = useContext(GlobalSessionContext);
-	const { handleResponse } = useHandleResponse();
 	const [classObject, setClassObject] = useState({});
 	const [classLoaded, setClassLoaded] = useState(false);
 
 	useEffect(async () => {
 		if (router?.query?.id) {
-			const DUMMY_STATUS = "succeeded";
-			const inputs = { profileId: globalSession.profileId, schoolId: globalSession.groups[globalSession.recentGroups[0]].id, classId: router.query.id };
-			let data = {};
-			try {
-				data = (await axios.post("/api/classes/fetch-one", { PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY, input: inputs, status: DUMMY_STATUS }))["data"];
-			} catch (error) {
-				data.status = "error";
-			} finally {
-				console.log(data);
-				handleResponse({
-					data,
-					failHandler: () => {
-						if (data.content === "not found") {
-							router.replace("/404");
-						}
-					},
-					successHandler: () => {
-						setClassObject(data.content);
-						setClassLoaded(true);
-					},
-				});
-			}
+			await post({
+				route: "/api/classes/fetch-one",
+				input: { profileId: globalSession.profileId, schoolId: globalSession.groups[globalSession.recentGroups[0]].id, classId: router.query.id },
+				failHandler: (data) => {
+					if (data.content === "not found") {
+						router.replace("/404");
+					}
+				},
+				successHandler: (data) => {
+					setClassObject(data.content);
+					setClassLoaded(true);
+				},
+			});
 		}
 	}, [router]);
 
