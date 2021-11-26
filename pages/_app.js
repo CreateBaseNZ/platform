792d29feafc4d639fe56820d4547e576ede98b3b
Louis <clin750@aucklanduni.ac.nz>
2021-11-27 00:39:56 +1300

    feat: mobile view

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,18 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import LoadingScreen from "../components/UI/Loading";
 import { SessionProvider } from "next-auth/react";
+import { io } from "socket.io-client";
 import { GlobalSessionContextProvider } from "../store/global-session-context";
-
-import "../styles/globals.scss";
 import { VisualBellContextProvider } from "../store/visual-bell-context";
-import VisualBell from "../components/VisualBell";
-import { useRouter } from "next/router";
-import AuthGuard from "../components/Auth/AuthGuard";
 import { ClassesContextProvider } from "../store/classes-context";
 import { MainLayoutContextProvider } from "../store/main-layout-context";
+import LoadingScreen from "../components/UI/Loading";
+import VisualBell from "../components/VisualBell";
+import MobileView from "../components/MobileView/MobileView";
+import AuthGuard from "../components/Auth/AuthGuard";
 
-import { io } from "socket.io-client";
+import "../styles/globals.scss";
 
 // function MyApp({ Component, pageProps }) {
 // 	const [loaded, setLoaded] = useState(false);
-// 	const [blockView, setBlockView] = useState(true);
-
-// 	useEffect(() => {
-// 		if (window && !window.matchMedia("only screen and (max-width: 760px)").matches) {
-// 			setBlockView(false);
-// 		}
-// 	}, []);
 
 // 	return (
 // 		<Provider session={pageProps.session}>
@@ -32,23 +22,6 @@ import { io } from "socket.io-client";
 // 					<div id="ctx-menu-root"></div>
 // 					{!loaded && <LoadingScreen />}
 // 					{!blockView && <Component {...pageProps} setLoaded={setLoaded} />}
-// 					{blockView && (
-// 						<div className="mobileView">
-// 							<h1>
-// 								We're sorry but <br />
-// 								<b>mobile view is currently unsupported.</b>
-// 							</h1>
-// 							<h2>To enjoy our platform, try viewing it on a desktop device or laptop.</h2>
-// 							<h3>
-// 								While you're here, why not check out our <a href="https://createbase.co.nz/">website</a>, which{" "}
-// 								<b>
-// 									<em>is</em>
-// 								</b>{" "}
-// 								supported on all devices 👏
-// 							</h3>
-// 							<img src="https://raw.githubusercontent.com/CreateBaseNZ/public/main/mobile.png" />
-// 						</div>
-// 					)}
 // 				</InviteOrgContextProvider>
 // 				<VisualBell />
 // 			</VisualBellContextProvider>
@@ -89,8 +62,9 @@ const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
 				<GlobalSessionContextProvider>
 					<ClassesContextProvider>
 						<MainLayoutContextProvider>
-							<div id="modal-root"></div>
 							{Component.auth ? <AuthGuard auth={Component.auth}>{getLayout(<Component {...pageProps} />)}</AuthGuard> : getLayout(<Component {...pageProps} />)}
+							<div id="modal-root"></div>
+							<MobileView />
 							<VisualBell />
 						</MainLayoutContextProvider>
 					</ClassesContextProvider>
