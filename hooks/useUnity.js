322fc0f01ef877deb1ed11fd57f0ea4025beef38
Brydon Burnett <brydon.burnett@gmail.com>
2021-12-30 22:10:06 +1300

    ?

--- hooks/useUnity.js
+++ hooks/useUnity.js
@@ -37,7 +37,7 @@ const useUnity = ({ scenePrefix, suffix, index, project, wip, setLoaded }) => {
 		});
 	}, []);
 
-	const sceneName = suffix ? `${scenePrefix}_${index},${suffix}` : `${scenePrefix}_${index}`;
+	const sceneName = isImprove ? `${scenePrefix}_${index},improve` : `${scenePrefix}_${index},manual`;
 
 	useEffect(() => {
 		console.log("loading");
