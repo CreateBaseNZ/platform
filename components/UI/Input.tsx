df37a8383a9224cdc6aa7bbb74eb09c3707c8342
Louis <clin750@aucklanduni.ac.nz>
2021-12-31 21:00:40 +1300

    chore: input tsx

--- components/UI/Input.tsx
+++ components/UI/Input.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import classes from "./Input.module.scss";
 
-const Input = ({ className = "", inputProps = {}, label, labelProps = {}, error, icon, ...rest }) => {
+interface IInputProps {
+	className?: string;
+	inputProps?: any; // TODO
+	label?: string;
+	labelProps?: any; // TODO
+	error?: {
+		message: string;
+	};
+	icon?: any; // TODO
+}
+
+const Input = ({ className, inputProps, label, labelProps, error, icon, ...rest }: IInputProps): JSX.Element => {
 	return (
 		<div {...rest} className={`${classes.inputWrapper} ${className} ${error ? classes.hasError : ""}`}>
 			<p className={classes.error}>{error ? error.message : ""}</p>
@@ -18,15 +29,13 @@ const Input = ({ className = "", inputProps = {}, label, labelProps = {}, error,
 
 export default Input;
 
-export const PasswordInput = ({ className, inputProps, label, error, ...rest }) => {
+export const PasswordInput = ({ className, inputProps, ...rest }: IInputProps): JSX.Element => {
 	const [show, setShow] = useState(false);
 
 	return (
 		<Input
 			className={`${className} ${classes.password}`}
 			inputProps={{ ...inputProps, type: show ? "text" : "password" }}
-			label={label}
-			error={error}
 			icon={
 				<>
 					<i className="material-icons" style={{ opacity: show ? 0.75 : 0, zIndex: show ? 2 : 1 }} title="Hide password" onClick={() => setShow((state) => !state)}>
@@ -42,7 +51,7 @@ export const PasswordInput = ({ className, inputProps, label, error, ...rest }) 
 	);
 };
 
-export const TextArea = ({ className = "", inputProps = {}, label, labelProps = {}, error, ...rest }) => {
+export const TextArea = ({ className, inputProps, label, labelProps, error, ...rest }: IInputProps): JSX.Element => {
 	return (
 		<div {...rest} className={`${classes.inputWrapper} ${className} ${error ? classes.hasError : ""}`}>
 			<p className={classes.error}>{error ? error.message : ""}</p>
@@ -54,15 +63,6 @@ export const TextArea = ({ className = "", inputProps = {}, label, labelProps = 
 	);
 };
 
-export const SearchBar = ({ className = "", inputProps = {}, label, labelProps = {}, error, ...rest }) => {
-	return (
-		<Input
-			className={`${className} ${classes.searchBar}`}
-			inputProps={{ ...inputProps, type: "text" }}
-			label={label}
-			error={error}
-			icon={<i className="material-icons-outlined">search</i>}
-			{...rest}
-		/>
-	);
+export const SearchBar = ({ className, inputProps, ...rest }: IInputProps): JSX.Element => {
+	return <Input className={`${className} ${classes.searchBar}`} inputProps={{ ...inputProps, type: "text" }} icon={<i className="material-icons-outlined">search</i>} {...rest} />;
 };
