447b536845460d9113b5bfa9cafa0b6e8cf1fe59
Louis <clin750@aucklanduni.ac.nz>
2022-01-25 00:13:35 +1300

    feat: header menus

--- components/Layouts/ProjectLayout/NewProjectLayout.tsx
+++ components/Layouts/ProjectLayout/NewProjectLayout.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactElement, useContext } from "react";
+import { Fragment, ReactElement, useContext, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import classes from "./NewProjectLayout.module.scss";
@@ -6,6 +6,8 @@ import STEPS from "../../../constants/projectSteps";
 import { useRouter } from "next/router";
 import UserAvatar from "../../UI/UserAvatar";
 import GlobalSessionContext from "../../../store/global-session-context";
+import getMainTabs from "../../../lib/getMainTabs";
+import { NEW_DEFAULT_TABS } from "../../../constants/mainTabs";
 
 interface Props {
 	children: ReactElement;
@@ -15,17 +17,37 @@ interface Props {
 const NewProjectLayout = ({ children, step }: Props) => {
 	const router = useRouter();
 	const { globalSession } = useContext(GlobalSessionContext);
+	const [showMenu, setShowMenu] = useState(false);
 
-	console.log(step);
+	console.log(showMenu);
 
 	return (
 		<div className={classes.container}>
 			<nav className={classes.nav}>
-				<div className={classes.logo}>
-					<button className={classes.mainMenu}>
+				<div className={`${classes.logo} ${showMenu ? classes.active : ""}`} tabIndex={-1} onBlur={() => setShowMenu(false)}>
+					<button className={classes.menuBtn} onClick={() => setShowMenu((state) => !state)} title="Open menu">
 						<Image src="https://raw.githubusercontent.com/CreateBaseNZ/public/dev/icons/logo-no-text.svg" height={32} width={28} alt="CreateBase" />
 						<i className="material-icons-outlined">expand_more</i>
 					</button>
+					<div className={classes.menu}>
+						{getMainTabs(globalSession.groups[globalSession.recentGroups[0]])[0].map((tab) => (
+							<Link key={tab.page} href={tab.urlObject}>
+								<a title={tab.label} onMouseDown={(e) => e.preventDefault()}>
+									<i className="material-icons-outlined">{tab.icon}</i>
+									{tab.label}
+								</a>
+							</Link>
+						))}
+						<div className={classes.sep} />
+						{NEW_DEFAULT_TABS.map((tab) => (
+							<Link key={tab.page} href={tab.urlObject}>
+								<a title={tab.label} onMouseDown={(e) => e.preventDefault()}>
+									<i className="material-icons-outlined">{tab.icon}</i>
+									{tab.label}
+								</a>
+							</Link>
+						))}
+					</div>
 				</div>
 				<div className={classes.title}>TO DO</div>
 				<div className={classes.steps}>
@@ -41,13 +63,18 @@ const NewProjectLayout = ({ children, step }: Props) => {
 					))}
 				</div>
 				<div className={classes.buttons}>
-					<div className={classes.group}>
-						<i className="material-icons-outlined">backpack</i>CreateBase School
-					</div>
 					<button className={classes.upgrade} title="Upgrade">
 						<i className="material-icons-outlined">bolt</i>
 					</button>
-					<UserAvatar id={globalSession.accountId} size={40} className={classes.avatar} />
+					<div className={classes.avatarWrapper}>
+						<UserAvatar id={globalSession.accountId} size={40} className={classes.avatar} />
+						<div className={classes.avatarTooltip}>
+							<span>
+								<b>{globalSession.groups[globalSession.recentGroups[0]].alias || `${globalSession.firstName} ${globalSession.lastName}`} </b> •{" "}
+								{globalSession.groups[globalSession.recentGroups[0]].name} {globalSession.groups[globalSession.recentGroups[0]].role}
+							</span>
+						</div>
+					</div>
 				</div>
 			</nav>
 			{children}
--- constants/mainTabs.ts
+++ constants/mainTabs.ts
@@ -2,22 +2,23 @@ import { IMainLayoutTab } from "../types/layouts";
 
 export const BROWSE: IMainLayoutTab = { urlObject: { pathname: "/browse" }, page: "browse", label: "Browse", icon: "view_carousel" };
 
-const CLASSES: IMainLayoutTab = { urlObject: { pathname: "/classes" }, page: "classes", label: "Classes", icon: "chair_alt" };
-const MANAGE_GROUP: IMainLayoutTab = { urlObject: { pathname: "/manage-group" }, page: "manage-group", label: "Manage Group", icon: "admin_panel_settings" };
+export const CLASSES: IMainLayoutTab = { urlObject: { pathname: "/classes" }, page: "classes", label: "Classes", icon: "chair_alt" };
+// TODO - change to HQ
+export const HQ: IMainLayoutTab = { urlObject: { pathname: "/manage-group" }, page: "manage-group", label: "Manage Group", icon: "admin_panel_settings" };
 
-const MY_GROUPS: IMainLayoutTab = { urlObject: { pathname: "/my-groups" }, page: "my-groups", label: "My Groups", icon: "groups" };
+export const MY_GROUPS: IMainLayoutTab = { urlObject: { pathname: "/my-groups" }, page: "my-groups", label: "My Groups", icon: "groups" };
 const ONBOARDING: IMainLayoutTab = { urlObject: { pathname: "/onboarding" }, page: "onboarding", label: "Onboarding", icon: "skateboarding" };
-const MY_ACCOUNT: IMainLayoutTab = { urlObject: { pathname: "/my-account" }, page: "my-account", label: "My Account", icon: "person" };
+export const ACCOUNT: IMainLayoutTab = { urlObject: { pathname: "/my-account" }, page: "my-account", label: "My Account", icon: "person" };
 const SUPPORT: IMainLayoutTab = { urlObject: { pathname: "/support" }, page: "support", label: "Support", icon: "support" };
-const INBOX: IMainLayoutTab = { urlObject: { pathname: "/inbox" }, page: "inbox", label: "Inbox", icon: "inbox" };
+export const INBOX: IMainLayoutTab = { urlObject: { pathname: "/inbox" }, page: "inbox", label: "Inbox", icon: "inbox" };
 
-const DEFAULT_TABS = [ONBOARDING, MY_GROUPS, INBOX, MY_ACCOUNT, SUPPORT];
+const DEFAULT_TABS = [ONBOARDING, MY_GROUPS, INBOX, ACCOUNT, SUPPORT];
 export default DEFAULT_TABS;
 
 export const MAIN_TABS = {
 	school: {
-		admin: [CLASSES, MANAGE_GROUP],
-		teacher: [CLASSES, MANAGE_GROUP],
+		admin: [CLASSES, HQ],
+		teacher: [CLASSES, HQ],
 		student: [],
 	},
 	family: {
@@ -25,3 +26,9 @@ export const MAIN_TABS = {
 		member: [],
 	},
 };
+
+// TODO - change browse to projects and phase out browse
+export const PROJECTS: IMainLayoutTab = { urlObject: { pathname: "/browse" }, page: "browse", label: "Projects", icon: "grid_view" };
+
+// TODO - replace
+export const NEW_DEFAULT_TABS = [MY_GROUPS, INBOX, ACCOUNT];
