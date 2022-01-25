8214f293944a00779d73bc2605389883393a97d8
Louis <clin750@aucklanduni.ac.nz>
2022-01-25 18:57:34 +1300

    feat: review rework

--- pages/project/[id]/review.tsx
+++ pages/project/[id]/review.tsx
@@ -1,10 +1,9 @@
 import { ReactElement } from "react";
-import Img from "../../../components/UI/Img";
 import NewProjectLayout from "../../../components/Layouts/ProjectLayout/NewProjectLayout";
 import { IProjectReadOnly } from "../../../types/projects";
 import { ALL_PROJECTS_ARRAY, ALL_PROJECTS_OBJECT } from "../../../constants/projects";
 
-import classes from "/styles/review.module.scss";
+import classes from "../../../styles/review.module.scss";
 
 interface Props {
 	data: IProjectReadOnly;
@@ -12,36 +11,29 @@ interface Props {
 
 const Review = ({ data }: Props) => {
 	return (
-		<div className={`${classes.view} roundScrollbar`}>
-			<div className={classes.imgContainer}>
-				<div className={classes.imgWrapper}>
-					<Img src="https://raw.githubusercontent.com/CreateBaseNZ/public/dev/project-pages/review.svg" layout="fill" objectFit="contain" label="Illustration by Storyset" />
-				</div>
-				<div className={classes.caption}>
-					Share your thoughts and ideas with your friends and teacher. How did everything go? Were you able to complete all the challenges? How did you overcome any problems?
-				</div>
+		<div className={`${classes.page} roundScrollbar`}>
+			<div className={classes.caption}>
+				Share your thoughts and ideas with your friends and teacher. How did everything go? Were you able to complete all the challenges? How did you overcome any problems?
 			</div>
-			<div className={classes.cardContainer}>
-				<div className={`${classes.cardWrapper} ${classes.survey}`}>
-					<span className="material-icons-outlined">thumb_up</span>
-					<div className={classes.card}>
-						<h3>Complete our survey</h3>
-						Let us know how you found this project so we can make future projects even better
-					</div>
-					<a href="https://forms.gle/x9dXkBKe2JoewnHH8" target="_blank" title="CreateBase workshop survey">
-						Complete our survey
-					</a>
+			<div className={`${classes.cardWrapper} ${classes.survey}`}>
+				<i className="material-icons-outlined">thumb_up</i>
+				<div className={classes.card}>
+					<h3>Complete our survey</h3>
+					Let us know how you found this project so we can make future projects even better
 				</div>
-				<div className={`${classes.cardWrapper} ${classes.message}`}>
-					<span className="material-icons-outlined">sms</span>
-					<div className={classes.card}>
-						<h3>Send us a message</h3>
-						Found a bug? Have some feedback? Just want to chat? We would love to hear from you
-					</div>
-					<a href="https://forms.gle/VJNUzpAhXG4KtiZz6" target="_blank" title="Send us a message!">
-						Get in touch
-					</a>
+				<a href="https://forms.gle/x9dXkBKe2JoewnHH8" target="_blank" title="CreateBase workshop survey" rel="noreferrer">
+					Complete our survey
+				</a>
+			</div>
+			<div className={`${classes.cardWrapper} ${classes.message}`}>
+				<i className="material-icons-outlined">sms</i>
+				<div className={classes.card}>
+					<h3>Send us a message</h3>
+					Found a bug? Have some feedback? Just want to chat? We would love to hear from you
 				</div>
+				<a href="https://forms.gle/VJNUzpAhXG4KtiZz6" target="_blank" title="Send us a message!" rel="noreferrer">
+					Get in touch
+				</a>
 			</div>
 		</div>
 	);
