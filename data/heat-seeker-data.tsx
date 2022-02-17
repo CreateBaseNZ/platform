0f0c6bf3cec1384034861b059fbbfd07d627e2a7
Brydon Burnett <brydon.burnett@gmail.com>
2022-02-17 15:01:36 +1300

    Heat Seeker docs + fixes

--- data/heat-seeker-data.tsx
+++ data/heat-seeker-data.tsx
@@ -21,7 +21,7 @@ const HEAT_SEEKER_DATA: IProjectReadOnly = {
 	caption:
 		"In this Project, learners will create an algorithm to guide a line-following robot to a series of fires within a warehouse, putting them out safely before they spread to nearby hydrogen fuel cells! Learners will not only create their own control-algorithm, but will also learn about some of the basics of fire safety, warehouse automation, and the advantage that robots have over humans when operating in hazardous situations.",
 	stacked: true,
-	documentation: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps",
+	documentation: "https://docs.google.com/document/d/1TdZrUo9KgamLL8vdphb51WV6P4lgU-JuxUyj3SIHFD8/edit?usp=sharing",
 	scenePrefix: "Project_HeatSeeker",
 	runType: "loop",
 	durPerLesson: "45 mins",
