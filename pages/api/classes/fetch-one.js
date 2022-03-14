eb05a550d142f1bc77877b88ba1b52eb97bc4c39
Carl Velasco <carlvelasco96@gmail.com>
2022-03-14 21:18:28 +1300

    Added Profile Id on return of api/classes/fetch-one

--- pages/api/classes/fetch-one.js
+++ pages/api/classes/fetch-one.js
@@ -84,6 +84,7 @@ const constructClass = (instance) => {
 			firstName: license.profile.name.first,
 			lastName: license.profile.name.last,
 			email: license.profile.account.local ? license.profile.account.local.email : license.profile.account.google.email,
+			profileId: license.profile,
 		};
 	});
 	return {
