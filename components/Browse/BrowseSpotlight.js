68e7d08e8129a2829f6bbffb25ce307f2c71ecd3
Louis <clin750@aucklanduni.ac.nz>
2022-03-16 15:15:03 +1300

    fix: non spotlight

--- components/Browse/BrowseSpotlight.js
+++ components/Browse/BrowseSpotlight.js
@@ -3,16 +3,20 @@ import classes from "./BrowseSpotlight.module.scss";
 const BrowseSpotlight = ({ project }) => {
 	return (
 		<div className={classes.spotlight}>
-			<div className={classes.wrapper}>
-				<iframe
-					src={project.spotlight}
-					title={project.name}
-					className={classes.iframe}
-					frameBorder="0"
-					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-					allowFullScreen
-				/>
-			</div>
+			{project.spotlight ? (
+				<div className={classes.wrapper}>
+					<iframe
+						src={project.spotlight}
+						title={project.name}
+						className={classes.iframe}
+						frameBorder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+					/>
+				</div>
+			) : (
+				"Coming soon!"
+			)}
 		</div>
 	);
 };
