9352f7aa49c50ec519769ca3b57e8ba97e136784
Brydon Burnett <brydon.burnett@gmail.com>
2022-03-19 16:53:23 +1300

    Removed text editor from Intro to Programming Project

--- data/intro-to-programming-data.tsx
+++ data/intro-to-programming-data.tsx
@@ -39,6 +39,7 @@ const INTRO_TO_PROGRAMMING_DATA: IProjectReadOnly = {
 	numOfLessons: 5,
 	difficulty: "advanced",
 	subjects: [TECHNOLOGY, COMPUTER_SCIENCE],
+	flowCodingOnly: true,
 	//spotlight: "https://www.youtube.com/embed/5A6Pz744DL8",
 	learningOutcome: "/intro-to-programming/files/project-overview-intro-to-programming.pdf",
 	cads: {
